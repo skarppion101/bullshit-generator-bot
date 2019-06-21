0d006e3469998b1e48ff52400d0fc70537176df6
Alex Semenov <skarppi@mail.ru>
2019-06-21 13:52:27 +0300

    Added proxy support

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,3 +1,5 @@
+// @ts-ignore
+import SocksAgent from "socks5-https-client/lib/Agent";
 import Telegraf, {ContextMessageUpdate} from "telegraf";
 import {Env} from "../utils/env";
 import {logError} from "../utils/logger";
@@ -5,7 +7,14 @@ import {handleCommands} from "./handlers/commands";
 import {loggerMiddleware} from "./middlewares/logger";
 
 export function createBot(env: Env): Telegraf<ContextMessageUpdate> {
-  const bot = new Telegraf(env.BOT_TOKEN);
+  const bot = new Telegraf(env.BOT_TOKEN, {
+    telegram: {
+      agent: new SocksAgent({
+        socksHost: "173.244.200.159",
+        socksPort: "36544",
+      }),
+    },
+  });
 
   bot.use(loggerMiddleware);
 
